ca88e382316343d1dbb7e680fac3a35684a7bdf2
James C <you@example.com>
2022-05-16 20:50:58 -0700

    feat: add comments and console logs

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,14 @@
+// My first function :)
 function changeTextStyle() {
     noticeMe = document.querySelector(".welcome")
     noticeMe.style.color = "blue"
     noticeMe.style.fontSize = "100px"
 }
+
+/* This is my first multi-line comment
+in JavaScript... yay!
+
+Time to log my name and favorite food
+to the console B) */
+console.log("James Carbajal")
+console.log("My favorite food is Sushi")
